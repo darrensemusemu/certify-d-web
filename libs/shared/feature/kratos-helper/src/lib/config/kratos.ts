beea3ef8227cc152cff07de21514d759ce6ae779
Darren Semusemu <47727831+darrensemusemu@users.noreply.github.com>
2022-09-07 22:20:08 +0200

    feat(kratos-helper) sert kratos base path (#19)
    feat(kratos-helper): set base path

--- libs/shared/feature/kratos-helper/src/lib/config/kratos.ts
+++ libs/shared/feature/kratos-helper/src/lib/config/kratos.ts
@@ -1,7 +1,7 @@
 import { Configuration, V0alpha2Api } from '@ory/kratos-client';
 
 export const kratosConfig = {
-  basePath: 'https://localhost/api/kratos/public',
+  basePath: 'https://certify-d.darrensemusemu.dev/api/kratos/public',
 };
 
 export default new V0alpha2Api(
