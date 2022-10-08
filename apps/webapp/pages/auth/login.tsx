1fe1f5151d91c2a63928ac49d06c497d2ee4a362
Darren Semusemu <darrensemusemu@gmail.com>
2022-10-08 23:32:55 +0200

    feat(webapp): add nextjs basepath

--- apps/webapp/pages/auth/login.tsx
+++ apps/webapp/pages/auth/login.tsx
@@ -1,8 +1,23 @@
 import { Login } from '@certify-d/kratos-helper';
+import { Link } from '@certify-d/shared-ui';
 import { NextPage } from 'next';
+import NextLink from 'next/link';
 
 const LoginPage: NextPage = () => {
-  return <Login />;
+  return (
+    <Login
+      createAccountBtn={({ href, passHref }) => (
+        <NextLink href={href} passHref={passHref}>
+          <Link>Create account</Link>
+        </NextLink>
+      )}
+      registerAccountBtn={({ href, passHref }) => (
+        <NextLink href={href} passHref={passHref}>
+          <Link>Recover your account</Link>
+        </NextLink>
+      )}
+    />
+  );
 };
 
 export default LoginPage;
